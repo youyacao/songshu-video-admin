bb7ae1e49b10714752b12d9d1a064cc09cd0e3e6
IvanZhuo <422108995@qq.com>
2021-03-21 18:14:04 +0800

    fix

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'mysql',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'videofree',
     // 用户名
-    'username'        => 'test',
+    'username'        => 'videofree',
     // 密码
-    'password'        => 'Zt123456',
+    'password'        => 'a8mCpcEa4Ktz2Afz',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
